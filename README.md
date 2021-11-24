f41bb8176068c2f1eb84d48e0117a1d32930f39c
Riju-vv <riju.vv@progress.com>
2021-11-24 05:12:43 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# haproxy_loadbalancer
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: haproxy_loadbalancer
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.

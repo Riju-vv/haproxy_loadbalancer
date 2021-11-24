6b0db6171485fc05d4c1bf33d51017ccb4813cd7
Riju-vv <riju.vv@progress.com>
2021-11-24 05:58:11 +0000

    End Of Lab: Launching Loadbalancer and Webserver VMs

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,13 +13,11 @@ haproxy_frontend 'http-in' do
   default_backend 'servers'
 end
 
-web_node_objects = search('node', "policy_name:webserver AND
-policy_group:#{node['policy_group']}")
+web_node_objects = search('node', "policy_name:webserver AND policy_group:#{node['policy_group']}")
 
 web_servers = []
 web_node_objects.each do |one_node|
-  server = "#{one_node['cloud']['public_hostname']}
-  #{one_node['cloud']['public_ipv4']}:80 check"
+  server = "#{one_node['cloud']['public_hostname']} #{one_node['cloud']['public_ipv4']}:80 check"
   web_servers.push(server)
 end
 # NOTE: the line where you set the "server" variable in the .each loop
